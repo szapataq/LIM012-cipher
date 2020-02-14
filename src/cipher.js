390e1ead3e03690c02ce2e7429893b38242c399d
szapataq <szapata013@gmail.com>
2020-02-14 11:36:01 -0500

    It is the first version of the cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,76 @@
+/*const cipher = {
+
+  encode:(offset,string)=>{
+    let mensaje=string.toUpperCase();
+    let clave = parseInt(offset);
+
+    let salida = "";
+    let letras_Ascii;
+    for(let i = 0; i < mensaje.length;i++){
+      let mensajeToAscii=mensaje[i].charCodeAt();
+        letras_Ascii = (mensajeToAscii - 65 + clave ) % 26 + 65;//formula sugerida a travez de codigo ASCII
+        salida = salida+ String.fromCharCode( letras_Ascii );
+    
+      
+    }
+    return salida;
+   
+    
+  },
+  decode:(offset,string)=>{
+    let mensaje=string.toUpperCase();
+    let clave=parseInt(offset);
+    let salida = "";
+    let letras_Ascii;
+ 
+    for(let i = 0; i < mensaje.length;i++){
+      letras_Ascii = (mensaje[ i ].charCodeAt() + 65 - clave ) % 26 + 65;//formula sugerida a travez de codigo ASCII
+      salida = salida+ String.fromCharCode( letras_Ascii);
+    }
+  return salida;
+  }
+
+  
+};
+
+export default cipher;*/
+
 const cipher = {
-  // ...
+
+  encode:(offset,string)=>{
+    let mensaje=string.toUpperCase();
+    let clave = parseInt(offset);
+
+    let salida = "";
+    let letras_Ascii;
+    for(let i = 0; i < mensaje.length ; i++){
+      let mensajeToAscii = mensaje[ i ].charCodeAt();
+      if(mensajeToAscii === 32){
+        salida=salida+String.fromCharCode( mensajeToAscii );
+      }else{
+        letras_Ascii = (mensajeToAscii - 65 + clave ) % 26 + 65;//formula sugerida a travez de codigo ASCII
+        salida = salida+ String.fromCharCode( letras_Ascii );
+      }
+    }
+    return salida;
+  },
+  decode:(offset,string)=>{
+    let mensaje=string.toUpperCase();
+    let clave = parseInt(offset);
+
+    let salida = "";
+    let letras_Ascii;
+    for ( let i = 0; i < mensaje.length ; i++ ){
+      let mensajeToAscii = mensaje[ i ].charCodeAt();
+      if (mensajeToAscii === 32){
+        salida=salida+String.fromCharCode(mensajeToAscii);
+      }else{
+        letras_Ascii = (mensajeToAscii + 65 - clave ) % 26 + 65;//formula sugerida a travez de codigo ASCII
+        salida = salida+ String.fromCharCode( letras_Ascii );
+      }
+    }
+    return salida;
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//**variables para encriptar */
+let mensaje = document.getElementById('mensaje');
+let clave = document.getElementById('clave');
+let btn = document.getElementById('btn');
+//**variables para desencriptar */
+let mensajeEnc = document.getElementById('mensajeEnc');
+let claveEnc = document.getElementById('claveEnc');
+let btnEnc = document.getElementById('btnEnc');
+
+btn.addEventListener("click", () => { 
+  let encrip = document.getElementById('encriptado');
+  encrip.value=cipher.encode(clave.value,mensaje.value);
+});
+btnEnc.addEventListener("click", () => {
+  let desencrip = document.getElementById('desencriptado');
+  desencrip.value=cipher.decode(claveEnc.value,mensajeEnc.value);
+});
+
+
+
+
